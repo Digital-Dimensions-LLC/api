aa6a7eacbc1bf19b0f7750ade5d99a5476438d7f
Justyn Freeman <justyn.christopher.freeman@gmail.com>
2021-11-01 13:53:28 -0400

    Create db.js

---
+++ db.js
@@ -0,0 +1,18 @@
+module.exports = {
+    async start() {
+
+let Keyv = require('keyv')
+        
+console.log('Databases Ready')
+      
+// Ban Database(s)
+global.bans = new Keyv('sqlite://.db/banlist.sqlite', { table: 'banlist', namespace: 'user'})
+global.reason = new Keyv('sqlite://.db/banlist.sqlite', { table: 'banlist', namespace: 'reason'})
+global.proof = new Keyv('sqlite://.db/banlist.sqlite', { table: 'banlist', namespace: 'proof'})
+global.moderator = new Keyv('sqlite://.db/banlist.sqlite', { table: 'banlist', namespace: 'moderator'})
+global.notes = new Keyv('sqlite://.db/banlist.sqlite', { table: 'banlist', namespace: 'notes'})
+global.timestamp = new Keyv('sqlite://.db/banlist.sqlite', { table: 'banlist', namespace: 'timestamp'})
+        
+// Authentication Databases
+global.authorizations = new Keyv('sqlite://.db/authorizations.sqlite', { table: 'authentication', namespace: 'token'})
+}}
