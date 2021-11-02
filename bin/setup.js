8cd0b9fcccef39ae60f6993b31997d23a32653c6
Justyn Freeman <justyn.c.freeman@gmail.com>
2021-11-02 11:55:45 -0400

    feat: scripts
    Add log setup and modification system to setup and start scripts

--- bin/setup.js
+++ bin/setup.js
@@ -1,6 +1,19 @@
 var shell = require("shelljs");
+var fs = require('fs')
 console.log('Installing Additional Dependencies')
 shell.exec("npm install express body-parser moment response-time keyv @keyv/sqlite");
 console.log('\n\n')
 console.log('Dependencies Installed')
+console.log('\n')
+console.log('Finishing Setup')
+let time = Date.now()
+let dir = './.azrael/logs/'
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir, { recursive: true });
+}
+fs.writeFileSync('./.azrael/' + 'log' + '.json', '{"id": "' + '0' + '"}', err => {
+    console.log(err);
+});
+console.log('Setup Completed')
+console.log('\n')
 console.log('Please run npm start')
--- bin/start.js
+++ bin/start.js
@@ -1,3 +1,18 @@
+var fs = require('fs')
+var moment = require('moment')
+
+let time = moment().format('LLLL')
+let logJSON = require('../.azrael/log.json')
+let lastID = logJSON.id
+let latestID = parseInt(logJSON.id) + 1
+
+fs.writeFileSync('./.azrael/' + 'log' + '.json', '{"id": "' + latestID + '"}', err => {
+    console.log(err);
+});
+fs.writeFileSync('./.azrael/logs/' + time + '.txt', '! Begin Logs', err => {
+    	console.log(err);
+    });
+
 const db = require('../src/db.js')
 db.start()
 
